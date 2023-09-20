e7c3d14c7dc13a7193c7915bd555ef64aec6f43f
desarrollador-endi1 <103216712+desarrollador-endi1@users.noreply.github.com>
2023-09-20 09:57:30 -0500

    AG: segunda parte partición manzanas

---
+++ rutinas/01_preparacion_validacion/03_extension_manzanas.R
@@ -0,0 +1,64 @@
+#
+rm(list = ls())
+#
+library(tidyverse)
+library(sf)
+library(foreach)
+library(doParallel)
+
+registerDoParallel(2)
+
+source("rutinas/funciones/extpol/extpol_2.R") #Extiente polígonos disjuntos
+
+load("intermedios/lista_parroquias.RData")
+
+# primer for de provincia
+foreach(i=1:length(index),
+        .packages = c("tidyverse", "sf"))%dopar%{
+  if(file.exists(paste0("intermedios/01_preparacion_validacion/", 
+                        substr(index[i], 1, 2), "/", index[i],"/manzana.gpkg"))){
+    
+  manzana <- read_sf(paste0("intermedios/01_preparacion_validacion/", 
+                            substr(index[i], 1, 2), "/", index[i],"/manzana.gpkg")) %>% 
+    select(man, id_parroquia)
+  
+  zona <- read_sf(paste0("intermedios/01_preparacion_validacion/", 
+                         substr(index[i], 1, 2), "/", index[i] ,"/zona.gpkg")) %>% 
+    filter(substr(zon, 7, 9) != "999")
+  
+  rios <- read_sf(paste0("intermedios/01_preparacion_validacion/", 
+                         substr(index[i], 1, 2), "/", index[i] ,"/rios.gpkg"))
+  
+  index_zon <- unique(zona$zon)
+  
+  man_ext <- vector("list", 0)
+  
+  for(z in index_zon){
+    perfil <- zona %>% 
+      filter(zon == z)
+    
+    polext <- extpol(manzana %>% filter(substr(man, 1, 9) == z) , 
+                     perfil, id = "man", buf = 2, densidad = 0.5, lindero = rios) 
+    
+    man_ext[[z]] <- polext[[2]]
+    
+  }
+  
+  save(man_ext, 
+       file = paste0("intermedios/01_preparacion_validacion/", 
+                     substr(index[i], 1, 2), "/", index[i] ,"/manzanas_extendidas.RData"))
+  
+  man_ext <- do.call(rbind, man_ext)
+  
+  dir.create(paste0("productos/01_preparacion_validacion/", 
+                    substr(index[i], 1, 2)), showWarnings = F)
+  
+  dir.create(paste0("productos/01_preparacion_validacion/", 
+                    substr(index[i], 1, 2), "/", index[i]), showWarnings = F)
+  
+  write_sf(man_ext, paste0("productos/01_preparacion_validacion/", 
+                           substr(index[i], 1, 2), "/", index[i] ,"/manzanas_extendidas.gpkg"))
+  
+  rm(manzana, perfil, polext, puntos_donas, man_ext, rios)
+  }
+}
